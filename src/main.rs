3c861e4a6933fbd4341c88b50e085c3be814f295
bypassOS <joegoldberg110010100@gmail.com>
2026-08-02 22:35:21 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,10 @@ async fn download_file(id: i32) -> Result<String, String> {
     println!("[ID {}] Waiting for a response from server...", id);
     tokio::time::sleep(Duration::from_millis(random)).await;
 
-    let network_status = rand::thread_rng().gen_bool(0.2);
+    let network_status = rand::thread_rng().gen_bool(0.8);
     let download = if network_status {
         println!("[ID {}]Downloading a file...", id);
-        tokio::time::sleep(Duration::from_millis(5000)).await;
+        tokio::time::sleep(Duration::from_millis(4000)).await;
         Ok(format!("[ID {}]Successfully!", id))
     }else {
         Err(format!("[ID {}]The error of download of file[!!!]", id))
@@ -43,22 +43,22 @@ async fn main() {
     println!("{a}");
     println!("{b}");
     //==============================================================================
-    tokio::spawn(async move {
+    for i in 1..=5 {
         let tx1 = tx.clone();
-        for i in 1..=5 {
-            let download = download_file(i).await;
+        tokio::spawn(async move {
+            let download = tokio::time::timeout(Duration::from_millis(2000),download_file(i)).await;
             tx1.send((download, i)).await.unwrap();
-        }
-        drop(tx1);
-    });
+        });
+    }
+    drop(tx);
     while let Some((response, id)) = rx.recv().await{
         let response = match response {
-            Ok(msg) => msg,
-            Err(err) => err,
+            Ok(Ok(msg)) => msg,
+            Ok(Err(err)) => err,
+            Err(_) => format!("Error of timeout"),
         };
         println!("[ID {}]RESULT[!!!]", id);
         println!("[ID {}]Conclusion: {}",id, response);
         println!("");
     }
-
 }
