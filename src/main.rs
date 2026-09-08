de9bcc9ed4c63579c5a435f59ad0d7f61bab7e5a
bypassOS <265340571+bypassOS-dev@users.noreply.github.com>
2026-09-08 20:00:24 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, Write}};
+use std::{io::{self, Write}, mem::transmute};
 
 #[tokio::main]
 async fn main() {   
@@ -54,6 +54,7 @@ async fn choice_1 () {
         match tokio::fs::OpenOptions::new().write(true).create_new(true).open(&full_name).await {
             Ok(_) => {
                 println!("\n File successfully created!");
+                break;
             }
             Err(err) => {
                 println!("Error: {err}");
@@ -61,10 +62,55 @@ async fn choice_1 () {
             }
         }
     }
-    
+    print!("\n\nDo u want to add some world in your new deck? ");
+    std::io::stdout().flush().unwrap();
+
+    let mut yes_no = String::new();
+    io::stdin()
+        .read_line(&mut yes_no)
+        .expect("Read error");
+    let quest = yes_no.trim();
+    let lower_string = quest.to_lowercase();
+
+    if lower_string == "yes" || lower_string == "y" ||  lower_string == "" {
+        print!("Great!");
+        let mut counter = 1;
+        loop {
+            let mut word = String::new();
+            print!("Enter {counter} word or 'stop' if u wanna end: ");
+            io::stdin()
+                .read_line(&mut word)
+                .expect("Read error... Why I am writing it? It will never happen anyway....");
+            let normal_word = word.trim();
+            let normal_word = normal_word.to_lowercase();
+            std::io::stdout().flush().unwrap();
+
+            if let "stop" = normal_word.as_str() {
+                break;
+            }
+
+            print!("\nEnter translate this ({normal_word}) word: ");
+            let mut translate = String::new();
+
+            io::stdin()
+                .read_line(&mut translate)
+                .expect("Read error... Why I am writing it? It will never happen anyway....");
+            let normal_translate = translate.trim();
+            let normal_translate = normal_translate.to_lowercase();
+            std::io::stdout().flush().unwrap();
+
+            let text_to_append = format!("{counter}. {normal_word}: {normal_translate}\n");
+
+            counter += 1;
+        }
+    } else {
+        println!("Ok! u can change your choice later!");
+    }
 }
 async fn choice_2() {
-
+    println!("That's right! Repeatition is most important in education!");
+    println!("Your decks of card:")
+    
 }
 async fn choice_3() {
 
